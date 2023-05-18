2cc6f5428ae847a4afcfd296c63d89def1634fc8
Omer Aplatony <omerap12@gmail.com>
2023-05-18 19:58:20 +0300

    Load freq words

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+def load_freq_words(filename):
+    word_list = []
+    with open(filename,'r') as file:
+        for line in file:
+            line = line.strip()
+            if not line:
+                continue
+            word_list.append(line)
+    file.close()
+    return word_list
+
 if __name__ == '__main__':
-    print("Omer Aplatony")
+    filename = "dict.txt"
+    words = load_words(filename)
+    
